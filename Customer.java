d45faff3fef217dc829dfc188d31f65fd96c6194
dolnadolnadol <saiparnchollada@gmail.com>
2023-05-23 23:09:19 +0700

    Main

--- Customer.java
+++ Customer.java
@@ -1,12 +1,17 @@
-// import java.util.ArrayList;
-
 public class Customer extends AllOrder{
     private int tableNo;
     private Basket basket;
+    private Allmenu alme;
 
     Customer(int tableNo){
         this.tableNo = tableNo;
         basket = new Basket();
+    }
+    public void setAlme(Allmenu al) {
+        alme = al;
+    }
+    public Allmenu getAlme() {
+        return alme;
     }
     public int getTableNo() {
         return tableNo;
